6e17a1af80a2cf1a38723695fa3eec18adc19834
abbyllshr-stack <abby.ll.sh.r@gmail.com>
2026-07-09 10:40:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
 
-    API_URL: "https://script.google.com/macros/s/AKfycbxnfKzj1Fdd31lNaRgEveoz_Fk0z28tB61LhTRry6pFdZTfSZb3HXtzV0YF6opYdjoC/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbztlTUuHtTruNprDhWxYZb4u7L_bQndK3ORkNewSWZu_1jFwdojtkEmDnTCiwvnmX1m/exec",
 
     REFRESH_TIME: 2000,
 
